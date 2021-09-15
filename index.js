e58e71a265a9c2494f90d920c4759aac7c658b25
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-09-15 15:34:26 +0300

    addind chat

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 
 const dotenv =require('dotenv');
 dotenv.config();
-require('./src/server.js').startup(process.env.PORT);
+require('./src/server.js').startup(3000);
--- public/js/events.js
+++ public/js/events.js
@@ -1,8 +1,30 @@
 import helpers from './helpers.js';
+// import rtc from './rtc';
 
 window.addEventListener( 'load', () => {
 
-
+    let chatElem = document.querySelector( '#chat-pane' );
+        let mainSecElem = document.querySelector( '#main-section' );
+        if ( chatElem.classList.contains( 'chat-opened' ) ) {
+            chatElem.setAttribute( 'hidden', true );
+            mainSecElem.classList.remove( 'col-md-9' );
+            mainSecElem.classList.add( 'col-md-12' );
+            chatElem.classList.remove( 'chat-opened' );
+        }
+        else {
+            chatElem.attributes.removeNamedItem( 'hidden' );
+            mainSecElem.classList.remove( 'col-md-12' );
+            mainSecElem.classList.add( 'col-md-9' );
+            chatElem.classList.add( 'chat-opened' );
+        }
+        //remove the 'New' badge on chat icon (if any) once chat is opened.
+        setTimeout( () => {
+            if ( document.querySelector( '#chat-pane' ).classList.contains( 'chat-opened' ) ) {
+                helpers.toggleChatNotificationBadge();
+            }
+        }, 300 );
+   
+        // console.log("responseUserData",responseUserData);
     let roomName = "hospital";
     let yourName = "ibrahim";
 
